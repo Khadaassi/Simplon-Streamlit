dfabc1f9a48afe360cc6a2d26fba082c34dd2404
Khadaassi <aassikhadija@yahoo.fr>
2024-11-15 14:07:00 +0100

    Amélioration de la logique de soumission des réponses et ajout de la fonctionnalité de redémarrage du quiz

--- pages/Quiz.py
+++ pages/Quiz.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from config.json_utils import read_json
+from random import shuffle
 
 questions = read_json('config/data.json')
+# st.session_state["test"] = False
 
 # Initialize session state variables if they do not exist
 if 'current_question_index' not in st.session_state:
@@ -24,18 +26,23 @@ if st.session_state['current_question_index'] < len(questions):
         # Display multiple-choice answers
         user_answer = st.radio("Choose your answer:", question['Answers'], key=f"question_{st.session_state['current_question_index']}")
         
+        # value_button = "submit" if st.session_state["test"] else "next"
         # Submit button
         submitted = st.form_submit_button("Submit")
         
         if submitted:  # Check if the button was clicked
-            if user_answer == question['Correct Answer']:
+            if user_answer == question['Correct_Answer']:
                 st.success("Correct answer!")
+                st.session_state['current_question_index'] += 1
                 st.session_state['score'] += 1
+                st.rerun()
             else:
                 st.error("Wrong answer.")
-            
+                st.session_state['current_question_index'] += 1
+                st.rerun()
             # Move to the next question after submission
-            st.session_state['current_question_index'] += 1
+            # st.session_state['current_question_index'] += 1
+        # st.session_state["test"] = False if st.session_state["test"] else True
 
 else:
     st.write("Quiz completed!")
@@ -44,6 +51,7 @@ else:
         st.balloons()
 
 # Reset the quiz
-if st.button("Restart the quiz"):
-    st.session_state['current_question_index'] = 0
-    st.session_state['score'] = 0
+    if st.button("Restart the quiz"):
+        st.session_state['current_question_index'] = 0
+        st.session_state['score'] = 0
+        st.rerun()
